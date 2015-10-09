063d67a88f99c273e0cfece3ea31154370e2dcf8
nathanielsmith <nathaniel.smith@myemail.indwes.edu>
2015-10-09 02:43:33 +0000

    done

--- piggetty.py
+++ piggetty.py
@@ -4,6 +4,7 @@
 # Add your name to this
 
 # Define a function called piggy(string) that returns a string
+piggy = "string"
 
 def piggy(word):
 	
@@ -14,21 +15,37 @@ def piggy(word):
 	return pig
 
 # Open the file *getty.txt* for reading.  
+get = open("getty.txt","r")
 
 # Open a new file *piggy.txt* for writing.  
+piggyfile = open("piggy.txt","w")
 
 # Read the getty.txt file into a string.  
+getstr = get.read()
 
 # Strip out bad characters (, - .).  
+getstr = getstr.replace (',','')
+getstr = getstr.replace ('-','')
+getstr = getstr.replace ('.','')
 
 # Split the string into a list of words.  
+getlist = getstr.split()
 
 # Create a new empty string.  
+newstr = ""
 
 # Loop through the list of words, pigifying each one.  
+for word in getlist:
+	newword = word [::-1]
+	newstr = newstr + newword + " "
+	
 
 # Add the pigified word (and a space) to the new string.  
+outfile = open("piggy.txt","w")
 
 # Write the new string to piggy.txt.  
+print (newstr, getlist, outfile)
 
 # close the files.
+outfile.close()
+get.close()
